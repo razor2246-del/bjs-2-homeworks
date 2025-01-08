5bd96f82e1665857a6dc2d88eb76bf324e90bf4d
Peter Bortnitsky <turok3445@mail.ru>
2025-01-08 16:29:03 +0300

    update 1.base-concepts/task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = Math.pow(b, 2)-4*a*c;
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d) )/(2*a);
+    let x2 = (-b - Math.sqrt(d) )/(2*a);
+    arr = [x1 , x2]
+  }
+  else if (d == 0) {
+    let x1 = -b/(2*a);
+    arr = [x1]
+  }
+  else {
+    arr = []
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let P = (percent / 100) / 12;
+  let S = amount - contribution;
+  let pay = S * (P + (P / (Math.pow((1 + P), countMonths) - 1)));
+  let finalPay = pay * countMonths;
+  return +finalPay.toFixed(2);
 }
